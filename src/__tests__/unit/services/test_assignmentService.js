97ac842bf8aaecc984ba85d2f78ed1883dec8bac
Tomek Petrykowski <tomasz.petrykowski.20@gmail.com>
2025-06-03 01:46:33 +0200

    test: Test new assignmentService features

--- src/__tests__/unit/services/test_assignmentService.js
+++ src/__tests__/unit/services/test_assignmentService.js
@@ -5,8 +5,36 @@ const { runTests } = require("../../../utils/tests");
 
 const Tests = {
   createAssignments: () => {
-    const result = assignmentService.createAssignments(1);
+    const result = assignmentService.createAssignments({ registrationId: 1 });
     console.log("Create Assignment result:", result);
+  },
+
+  getResults: () => {
+    const result = assignmentService.getResults({ registrationId: 1 });
+    console.log("Get Results result:", result);
+  },
+
+  getResultsByParticipant: () => {
+    const result = assignmentService.getResults({
+      registrationId: 1,
+      participantId: 1,
+    });
+    console.log("Get Results By Participant result:", result);
+  },
+
+  getResultsByGroup: () => {
+    const result = assignmentService.getResults({
+      registrationId: 1,
+      groupId: 1,
+    });
+    console.log("Get Results By Group result:", result);
+  },
+
+  clearRegistration: () => {
+    const result = assignmentService.clearRegistration({ registrationId: 1 });
+    const registration = assignmentService.getResults({ registrationId: 1 });
+    console.log("Clear Registration result:", result);
+    console.log("Registration after clear:", registration);
   },
 };
 
